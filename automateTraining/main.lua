39b95a61a5107d5ed9f8a164172792304515ace7
Mahiethan <mahiethan21@gmail.com>
2024-09-03 23:25:31 +0100

    Implemented automatic spell casting
    Lua script now repeatedly casts the player's currently equipped spell when the F12 key is pressed.

--- automateTraining/main.lua
+++ automateTraining/main.lua
@@ -1,19 +1,56 @@
-gameLoaded = false
+gameLoaded = false -- Flag set when save game is loaded, a prerequisite to the automatic spell casting
+toggleTraining = false -- Flag which is set true or false, when the F12 key is pressed, in order to activate or deactivate the spell casting automation
 
-local function automateTraining()
-    tes3.messageBox("F12 key was pressed!") -- For testing purposes, remove if no longer needed
-
+local function castSpell()
     -- Create menu asking for which magic skill to train
 
     -- Once a magic school is selected, find all character spells under this school, and choose cheapest cost spell
 
     -- Equip spell, and activate it endlessly until magicka runs out
+
+        local player = tes3.player
+
+        local magicSlot = tes3.mobilePlayer.currentSpell -- Get current equipped magic spell
+
+        -- Check if player has enough magicka before casting spell
+
+        -- Check if the player has a spell readied
+        if magicSlot then
+            -- Cast the currently equipped spell
+            tes3.cast({
+                reference = player,    
+                target = tes3.getPlayerTarget(),
+                spell = magicSlot,
+                alwaysSucceeds = false
+            })
+            -- tes3.messageBox("Casting equipped spell: %s", magicSlot.id)
+        end
+end
+
+local function automateTraining()
+    castSpell()
+end
+
+-- Handles loop for repeated spell casting
+local function onEveryFrame(e)
+    if toggleTraining then
+        -- set toggleTraining to false if creating a menu, so that menu is not appearing repeatedly
+        automateTraining()
+    end
 end
 
 -- Function to check if the pressed key is F12, and to check if game is loaded, in order to activate the automation
 local function onKeyPress(e)
     if e.keyCode == 88 and gameLoaded then
-        automateTraining()
+
+        if toggleTraining then
+            toggleTraining = false -- Disable spell casting automation
+            tes3.messageBox("Deactivating training script.")
+        else
+            toggleTraining = true -- Enable spell casting automation
+            tes3.messageBox("Activating training script.")
+        end
+
     end
 end
 
@@ -23,7 +60,7 @@ local function onGameLoaded()
 end
 
 -- Function to set a flag whilst on a menu i.e. main menu, pause menu
-local function onMenu()
+local function enterMenu()
     gameLoaded = false
 end
 
@@ -33,5 +70,11 @@ event.register("keyDown", onKeyPress)
 -- Register event handler to check if a game save has been loaded
 event.register("loaded", onGameLoaded)
 
--- Register event handler to check if user is on main menu / is in process of loading
-event.register("menuEnter", onMenu)
+-- Register event handler to check if user is entering a menu
+event.register("menuEnter", enterMenu)
+
+-- Register event handler to check if user is exiting a menu
+event.register("menuExit", onGameLoaded)
+
+-- Register event handler to perform automated casting without the need for a 'while' loop
+event.register(tes3.event.simulate, onEveryFrame)  
